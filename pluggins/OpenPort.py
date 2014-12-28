9fafeca3e0ac46fee4ef9eb058fad9697a11542e
bplower <bplower@alaska.edu>
2014-12-27 17:23:42 -0900

    Updated OpenPort.py to work with the pluggin environment. This has been tested and works.

--- pluggins/OpenPort.py
+++ pluggins/OpenPort.py
@@ -1,17 +1,41 @@
+# Imports required for django modules
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cssefwebfront.settings'
+from django.conf import settings
+
+# Imports required for base pluggin
+from cssefwebfront.models import Score
+from ScoringUtils import Pluggin
+from ScoringUtils import PlugginTest
+
+# Imports required for specific pluggin
 import socket
 
-class OpenPort:
+class OpenPort(Pluggin):
+    team_config_type_dict = {
+        "port": int,
+        "network": str,
+        "timeout": int
+    }
     def __init__(self, conf_dict):
-        self.name = conf_dict["name"]
-        self.port = int(conf_dict["port"])
-        self.socket_timeout = int(conf_dict["socket_timeout"])
+        Pluggin.__init__(self, conf_dict)
 
     def score(self, team):
+        team_config = team.score_configs[self.__class__.__name__]
+        address = self.build_address(team_config)
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Sets low timeout otherwise scoring will take FOREVER
-        sock.settimeout(self.socket_timeout)
-        result = sock.connect_ex((team.net_addr, self.port))
+        sock.settimeout(team_config["timeout"])
+        result = sock.connect_ex((address, team_config["port"]))
+        new_score = Score()
         if result == 0:
-            return True
+            new_score.value = self.points
+            new_score.message = ""
         else:
-            return False
+            new_score.value = 0
+            new_score.message = "" # TODO: Could eventually use an error message here
+        return new_score
+
+class Test(PlugginTest):
+    def __init__(self):
+        PlugginTest.__init__(self, OpenPort)
