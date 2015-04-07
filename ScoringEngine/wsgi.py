dcc325239dc2556cad6eaf056ca50348ff293181
bplower <bplower@alaska.edu>
2015-04-07 14:31:04 -0800

    Removed old file locations. Moved files around incorrectly in the previous commit. Commited all the file deletions.

--- ScoringEngine/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cybersecweb.settings")
-
-import djcelery
-djcelery.setup_loader()
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
--- WebInterface/views/general.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.shortcuts import render
-from django.contrib import auth
-from django.core.context_processors import csrf
-
-def home(request):
-	return render_to_response('home.html', c)
