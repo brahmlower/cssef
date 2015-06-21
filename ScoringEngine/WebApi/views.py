4bf4d7c54abe3ca3f4ad5c41e552a1d82f127dc7
bplower <bplower@alaska.edu>
2015-06-20 17:38:16 -0800

    API Views modernizations Cleaned up the older code in the other api view methods. The views for individual objects (not lists) are now using the 'objectExists' and 'deleteObject' methods now. Both of those methods were tweaked slightly to make them applicable to the other view functions.

--- ScoringEngine/WebApi/views.py
+++ ScoringEngine/WebApi/views.py
@@ -40,11 +40,11 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-def competitionExists(competitionId):
+def objectExists(objectType, **kwargs):
     try:
-        Competition.objects.get(competitionId = competitionId)
+        objectType.objects.get(**kwargs)
         return True
-    except Competition.DoesNotExist:
+    except objectType.DoesNotExist:
         return False
 
 def listObject(objectType, objectTypeSerializer, **kwargs):
@@ -82,7 +82,7 @@ def patchObject(request, objectType, objectTypeSerializer, **kwargs):
         return JSONResponse(serializer.data, status = status.HTTP_202_ACCEPTED)
     return JSONResponse(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-def deleteObject(request, objectType, **kwargs):
+def deleteObject(objectType, **kwargs):
     objectType.objects.get(**kwargs).delete()
     return Response(status = status.HTTP_204_NO_CONTENT)
 
@@ -115,234 +115,185 @@ def competitions(request):
         return listObjects(Competition, CompetitionSerializer)
     elif request.method == 'POST':
         return postObject(request, CompetitionSerializer)
-    else:
-        return Response(status = status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'PATCH', 'DELETE'])
 def competition(request, competitionId):
-    if not competitionExists(competitionId):
+    if not objectExists(Competition, competitionId = competitionId):
         return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Competition, CompetitionSerializer, competitionId = competitionId)
     elif request.method == 'PATCH':
         return patchObject(request, Competition, CompetitionSerializer, competitionId = competitionId)
     elif request.method == 'DELETE':
-        return deleteObject(request, Competition, competitionId = competitionId)
+        return deleteObject(Competition, competitionId = competitionId)
 
-
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def teams(request, competitionId):
     if request.method == 'GET':
         return listObjects(Team, TeamSerializer)
     elif request.method == 'POST':
         return postObject(request, TeamSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def team(request, competitionId, teamId):
-    try:
-        Team.objects.get(teamId = teamId)
-    except Team.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Team, competitionId = competitionId, teamId = teamId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Team, TeamSerializer, teamId = teamId)
     elif request.method == 'POST':
         team = Team.objects.get(teamId = teamId)
         return postObject(request, TeamSerializer, team)
     elif request.method == 'DELETE':
-        Team.objects.get(teamId = teamId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(Team, teamId = teamId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def services(request, competitionId):
     if request.method == 'GET':
         return listObjects(Service, ServiceSerializer)
     elif request.method == 'POST':
         return postObject(request, ServiceSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def service(request, competitionId, serviceId):
-    try:
-        Service.objects.get(serviceId = serviceId)
-    except Service.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Service, competitionId = competitionId, serviceId = serviceId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Service, ServiceSerializer, serviceId = serviceId)
     elif request.method == 'POST':
         service = Service.objects.get(serviceId = serviceId)
         return postObject(request, ServiceSerializer, service)
     elif request.method == 'DELETE':
-        Service.objects.get(serviceId = serviceId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(Service, serviceId = serviceId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def scores(request, competitionId):
     if request.method == 'GET':
         return listObjects(Score, ScoreSerializer)
     elif request.method == 'POST':
         return postObject(request, ScoreSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def score(request, competitionId, scoreId):
-    try:
-        Score.objects.get(scoreId = scoreId)
-    except Score.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Score, competitionId = competitionId, scoreId = scoreId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Score, ScoreSerializer, scoreId = scoreId)
     elif request.method == 'POST':
         score = Score.objects.get(scoreId = scoreId)
         return postObject(request, ScoreSerializer, score)
     elif request.method == 'DELETE':
-        Score.objects.get(scoreId = scoreId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(Score, scoreId = scoreId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def injects(request, competitionId=None, injectId=None):
     if request.method == 'GET':
         return listObjects(Inject, InjectSerializer)
     elif request.method == 'POST':
         return postObject(request, InjectSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def inject(request, competitionId, injectId):
-    try:
-        Inject.objects.get(injectId = injectId)
-    except Inject.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Inject, competitionId = competitionId, injectId = injectId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Inject, InjectSerializer, injectId = injectId)
     elif request.method == 'POST':
         inject = Inject.objects.get(injectId = injectId)
         return postObject(request, InjectSerializer, inject)
     elif request.method == 'DELETE':
-        Inject.objects.get(injectId = injectId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(request, Inject, injectId = injectId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def injectresponses(request, competitionId):
     if request.method == 'GET':
         return listObjects(InjectResponse, InjectResponseSerializer)
     elif request.method == 'POST':
         return postObject(request, InjectResponseSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def injectresponse(request, competitionId, injectResponseId):
-    try:
-        InjectResponse.objects.get(injectResponseId = injectResponseId)
-    except InjectResponse.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(InjectResponse, competitionId = competitionId, injectResponseId = injectResponseId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(InjectResponse, InjectResponseSerializer, injectResponseId = injectResponseId)
     elif request.method == 'POST':
         injectresponse = InjectResponse.objects.get(injectResponseId = injectResponseId)
         return postObject(request, InjectResponseSerializer, injectresponse)
     elif request.method == 'DELETE':
-        InjectResponse.objects.get(injectResponseId = injectResponseId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(InjectResponse, injectResponseId = injectResponseId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def incidentresponses(request, competitionId):
     if request.method == 'GET':
         return listObjects(IncidentResponse, IncidentResponseSerializer)
     elif request.method == 'POST':
         return postObject(request, IncidentResponseSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def incidentresponse(request, competitionId, incidentResponseId):
-    try:
-        IncidentResponse.objects.get(incidentResponseId = incidentResponseId)
-    except IncidentResponse.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(IncidentResponse, competitionId = competitionId, incidentResponseId = incidentResponseId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(IncidentResponse, IncidentResponseSerializer, incidentResponseId = incidentResponseId)
     elif request.method == 'POST':
         incidentresponse = IncidentResponse.objects.get(incidentResponseId = incidentResponseId)
         return postObject(request, IncidentResponseSerializer, incidentresponse)
     elif request.method == 'DELETE':
-        IncidentResponse.objects.get(incidentResponseId = incidentResponseId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(IncidentResponse, incidentResponseId = incidentResponseId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def plugins(request):
     if request.method == 'GET':
         return listObjects(Plugin, PluginSerializer)
     elif request.method == 'POST':
         return postObject(request, PluginSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def plugin(request, pluginId):
-    try:
-        Plugin.objects.get(pluginId = pluginId)
-    except Plugin.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Plugin, pluginId = pluginId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Plugin, PluginSerializer, pluginId = pluginId)
     elif request.method == 'POST':
         plugin = Plugin.objects.get(pluginId = pluginId)
         return postObject(request, PluginSerializer, plugin)
     elif request.method == 'DELETE':
-        Plugin.objects.get(pluginId = pluginId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(Plugin, pluginId = pluginId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def users(request):
     if request.method == 'GET':
         return listObjects(User, UserSerializer)
     elif request.method == 'POST':
         return postObject(request, UserSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def user(request, userId):
-    try:
-        User.objects.get(userId = userId)
-    except User.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(User, userId = userId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(User, UserSerializer, userId = userId)
     elif request.method == 'POST':
         user = User.objects.get(userId = userId)
         return postObject(request, UserSerializer, user)
     elif request.method == 'DELETE':
-        User.objects.get(userId = userId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(User, userId = userId)
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def organizations(request):
     if request.method == 'GET':
         return listObjects(Organization, OrganizationSerializer)
     elif request.method == 'POST':
         return postObject(request, OrganizationSerializer)
-    else:
-        return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def organization(request, organizationId):
-    try:
-        Organization.objects.get(organizationId = organizationId)
-    except Organization.DoesNotExist:
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+    if not objectExists(Organization, organizationId = organizationId):
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         return listObject(Organization, OrganizationSerializer, organizationId = organizationId)
     elif request.method == 'POST':
         organization = Organization.objects.get(organizationId = organizationId)
         return postObject(request, OrganizationSerializer, organization)
     elif request.method == 'DELETE':
-        Organization.objects.get(organizationId = organizationId).delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return deleteObject(Organization, organizationId = organizationId)
