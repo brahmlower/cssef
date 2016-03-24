d3ecdbd82250648ec6234321d9ddfd51b38217e6
bplower <bplower@gmail.com>
2016-03-23 20:15:17 -0800

    Update reinstall script
    The scrip will now create an admin password, set it in the config file, and then use that while creating the admin user and admin group.

--- Cssef/reinstall.sh
+++ Cssef/reinstall.sh
@@ -29,11 +29,15 @@ sudo chmod go+w /var/log/cssef
 # the log file directory (we're not running with any elevated permissions)
 sudo sed -i 's|/var/run/|/tmp/|' /etc/cssef/cssefd.yml
 
+# We're going to set the admin key to use for the initial setup
+admintoken=`openssl rand -hex 16`
+sudo sed -i "s|admin-token:|admin-token: $admintoken|" /etc/cssef/cssefd.yml
+
 # Now restart the server
 cssefd start
 
 # Add an organization to add our user to.
-cssef-cli --admin-token abcdefg organization add --name=Administrators --maxMembers=10
+cssef-cli --admin-token $admintoken organization add --name=Administrators --maxMembers=10
 
 # And now our new test admin user!
-cssef-cli --admin-token abcdefg user add --organization=1 --name=Admin --username=admin --password=admin
+cssef-cli --admin-token $admintoken user add --organization=1 --name=Admin --username=admin --password=admin
