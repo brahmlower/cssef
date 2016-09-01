5840ad6c196d71c91a9de6a7117a232b84b84e37
bplower <bplower@gmail.com>
2016-08-31 22:52:05 -0800

    Updated server tests
    Updated a the tests related to the Login endpoint. They're working now, but the tests themselves aren't passing for the most part. I'll resolve those eventually. Also changed indentation from tabs to spaces for a couple files (not sure how that was missed before).

--- CssefServer/tests/tests_api_account.py
+++ CssefServer/tests/tests_api_account.py
@@ -6,44 +6,38 @@ from cssefserver.account.api import User
 
 class UserTest(CssefTest):
     def test_create_minimal_user(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         self.assertTrue(isinstance(user_object, User))
 
     def test_edit_user(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         user_object.edit(name = "Super Bob")
         self.assertEquals(user_object.name, "Super Bob")
 
     def test_delete_user(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         user_object.delete()
-        user_object1 = User.from_database(self.db_connection, user_object.get_id())
+        user_object1 = User.from_database(self.database_connection, user_object.get_id())
         self.assertEquals(user_object1, None)
 
     def test_auth_password_success(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         authed = user_object.authenticate_password("bobpass")
         self.assertTrue(authed)
 
     def test_auth_password_fail(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         authed = user_object.authenticate_password("notpass")
         self.assertFalse(authed)
 
     def test_auth_token_success(self):
-        user_object = create_org_and_user(self.db_connection)
-        token = user_object.get_new_token()
-        authed = user_object.authenticate_token(token)
-        self.assertTrue(authed)
-
-    def test_auth_token_success(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         token = user_object.get_new_token()
         authed = user_object.authenticate_token(token)
         self.assertTrue(authed)
 
     def test_auth_token_fail(self):
-        user_object = create_org_and_user(self.db_connection)
+        user_object = create_org_and_user(self.database_connection)
         token = "longfaketokenthatisntatoken"
         authed = user_object.authenticate_token(token)
         self.assertFalse(authed)
@@ -51,15 +45,15 @@ class UserTest(CssefTest):
 class OrganizationTest(CssefTest):
     def test_create_minimal_org(self):
         org_dict = { "name": "testorg" }
-        org_object = Organization.from_dict(self.db_connection, org_dict)
+        org_object = Organization.from_dict(self.database_connection, org_dict)
 
-def create_org_and_user(db_connection):
+def create_org_and_user(database_connection):
     org_dict = { "name": "testorg", "max_members": 10 }
-    org_object = Organization.from_dict(db_connection, org_dict)
+    org_object = Organization.from_dict(database_connection, org_dict)
     user_dict = {
         'username': "bob",
         "name": "Bob",
         "password": "bobpass",
         "organization": org_object.get_id()
     }
-    return User.from_dict(db_connection, user_dict)
+    return User.from_dict(database_connection, user_dict)
--- CssefServer/tests/tests_tasks_base.py
+++ CssefServer/tests/tests_tasks_base.py
@@ -1,45 +1,46 @@
 #!/usr/bin/python
 from . import CssefTest
-from cssefserver import tasks
+from cssefserver.account import tasks
 
 class Login(CssefTest):
-	def test_credential_success(self):
-		endpoint = tasks.Login(self.config, self.db_connection)
-		auth = {'username': "admin", 'password': "admin", 'organization': 1}
-		return_dict = endpoint(auth = auth)
-		expected_dict = {'content': ['Success'], 'message': [], 'value': 0}
-		self.assert_dict_content(return_dict, expected_dict)
+    def test_credential_success(self):
+        endpoint = tasks.Login(self.config, self.database_connection)
+        auth = {'username': "admin", 'password': "admin", 'organization': 1}
+        return_dict = endpoint(auth = auth)
+        expected_dict = {'content': ['Success'], 'message': [], 'value': 0}
+        self.assert_dict_content(return_dict, expected_dict)
 
-	def test_credential_fail_user(self):
-		endpoint = tasks.Login(self.config, self.db_connection)
-		auth = {'username': "fail", 'password': "admin", 'organization': 1}
-		return_dict = endpoint(auth = auth)
-		expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
-		self.assert_dict_content(return_dict, expected_dict)
+    def test_credential_fail_user(self):
+        endpoint = tasks.Login(self.config, self.database_connection)
+        auth = {'username': "fail", 'password': "admin", 'organization': 1}
+        return_dict = endpoint(auth = auth)
+        expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
+        self.assert_dict_content(return_dict, expected_dict)
 
-	def test_credential_fail_password(self):
-		endpoint = tasks.Login(self.config, self.db_connection)
-		auth = {'username': "admin", 'password': "fail", 'organization': 1}
-		return_dict = endpoint(auth = auth)
-		expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
-		self.assert_dict_content(return_dict, expected_dict)
+    def test_credential_fail_password(self):
+        endpoint = tasks.Login(self.config, self.database_connection)
+        auth = {'username': "admin", 'password': "fail", 'organization': 1}
+        return_dict = endpoint(auth = auth)
+        expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
+        self.assert_dict_content(return_dict, expected_dict)
 
-	def test_credential_fail_organization(self):
-		endpoint = tasks.Login(self.config, self.db_connection)
-		auth = {'username': "admin", 'password': "admin", 'organization': 9001}
-		return_dict = endpoint(auth = auth)
-		expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
-		self.assert_dict_content(return_dict, expected_dict)
+    def test_credential_fail_organization(self):
+        endpoint = tasks.Login(self.config, self.database_connection)
+        auth = {'username': "admin", 'password': "admin", 'organization': 9001}
+        return_dict = endpoint(auth = auth)
+        expected_dict = {'content': [], 'message': ['Incorrect username or password.'], 'value': 1}
+        self.assert_dict_content(return_dict, expected_dict)
 
-	def test_credential_no_password_with_token(self):
-		endpoint = tasks.Login(self.config, self.db_connection)
-		auth = {'username': "admin", 'token': "abc123", 'organization': 1}
-		return_dict = endpoint(auth = auth)
-		expected_dict = {'content': [], 'message': ['Token may not be used to log in.'], 'value': 2}
-		self.assert_dict_content(return_dict, expected_dict)
+    def test_credential_no_password_with_token(self):
+        # This unit test fails because the server throws an error while finding keyword 'password'. Error should ultimately be changed to say Login requires password.
+        endpoint = tasks.Login(self.config, self.database_connection)
+        auth = {'username': "admin", 'token': "abc123", 'organization': 1}
+        return_dict = endpoint(auth = auth)
+        expected_dict = {'content': [], 'message': ['Token may not be used to log in.'], 'value': 2}
+        self.assert_dict_content(return_dict, expected_dict)
 
 class RenewToken(CssefTest):
-	pass
+    pass
 
 class AvailableEndpoints(CssefTest):
-	pass
+    pass
