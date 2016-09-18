1f33f4cdfd6a635af7b177e94a02d593c71d00bb
bplower <bplower@gmail.com>
2016-09-17 22:03:16 -0800

    Removed obsolete get/set functions
    The previous commit drastically improved code re-use. Everything removed in this commit should still work as it did before.

--- plugins/cssefcdc/cssefcdc/api.py
+++ plugins/cssefcdc/cssefcdc/api.py
@@ -20,24 +20,6 @@ class ServicePlugin(ModelWrapper):
         'description'
     ]
 
-    @property
-    def name(self):
-        return self.model.name
-
-    @name.setter
-    def name(self, value):
-        self.model.name = value
-        self.db_conn.commit()
-
-    @property
-    def description(self):
-        return self.model.description
-
-    @description.setter
-    def description(self, value):
-        self.model.description = value
-        self.db_conn.commit()
-
     def get_module_name(self):
         #return Document.objects.get(servicemodule = self.servicemodule).filename.split(".")[0]
         pass
@@ -62,87 +44,6 @@ class Service(ModelWrapper):
         'machineFqdn',
         'defaultPort'
     ]
-
-    @property
-    def name(self):
-        return self.model.name
-
-    @name.setter
-    def name(self, value):
-        self.model.name = value
-        self.db_conn.commit()
-
-    @property
-    def description(self):
-        return self.model.description
-
-    @description.setter
-    def description(self, value):
-        self.model.description = value
-        self.db_conn.commit()
-
-    @property
-    def manual_start(self):
-        return self.model.manualStart
-
-    @manual_start.setter
-    def manual_start(self, value):
-        self.model.manualStart = value
-        self.db_conn.commit()
-
-    @property
-    def datetime_start(self):
-        return self.model.datetimeStart
-
-    @datetime_start.setter
-    def datetime_start(self, value):
-        self.model.datetimeStart = value
-        self.db_conn.commit()
-
-    @property
-    def datetime_finish(self):
-        return self.model.datetimeFinish
-
-    @datetime_finish.setter
-    def datetime_finish(self, value):
-        self.model.datetimeFinish = value
-        self.db_conn.commit()
-
-    @property
-    def points(self):
-        return self.model.points
-
-    @points.setter
-    def points(self, value):
-        self.model.points = value
-        self.db_conn.commit()
-
-    @property
-    def machine_ip(self):
-        return self.model.machineIp
-
-    @machine_ip.setter
-    def machine_ip(self, value):
-        self.model.machineIp = value
-        self.db_conn.commit()
-
-    @property
-    def machine_fqdn(self):
-        return self.model.machineFqdn
-
-    @machine_fqdn.setter
-    def machine_fqdn(self, value):
-        self.model.machineFqdn = value
-        self.db_conn.commit()
-
-    @property
-    def default_port(self):
-        return self.model.defaultPort
-
-    @default_port.setter
-    def default_port(self, value):
-        self.model.defaultPort = value
-        self.db_conn.commit()
 
     # I commented this out because it relied on the loadPlugin being there.
     # Haven't bothered to migrated that quite yet :)
@@ -172,24 +73,6 @@ class ScoringEngine(ModelWrapper):
     ]
 
     @property
-    def name(self):
-        return self.model.name
-
-    @name.setter
-    def name(self, value):
-        self.model.name = value
-        self.db_conn.commit()
-
-    @property
-    def enabled(self):
-        return self.model.enabled
-
-    @enabled.setter
-    def enabled(self, value):
-        self.model.enabled = value
-        self.db_conn.commit()
-
-    @property
     def package_name(self):
         # This is a read-only attribute, so there is no corresponding setter method
         return self.model.name
@@ -206,105 +89,6 @@ class Competition(ModelWrapper):
         'datetimeStart',
         'datetimeFinish',
         'autoStart']
-
-    @property
-    def organization(self):
-        return self.model.organization
-
-    @organization.setter
-    def organization(self, value):
-        # Todo:
-        # Only site admins should be able to change this
-        self.model.organization = value
-        self.db_conn.commit()
-
-    @property
-    def name(self):
-        return self.model.name
-
-    @name.setter
-    def name(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.name = value
-        self.db_conn.commit()
-
-    @property
-    def url(self):
-        return self.model.url
-
-    @url.setter
-    def url(self, value):
-        # Todo:
-        # Only site admins should be able to change this
-        self.model.url = value
-        self.db_conn.commit()
-
-    @property
-    def description(self):
-        return self.model.description
-
-    @description.setter
-    def description(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.description = value
-        self.db_conn.commit()
-
-    @property
-    def datetime_display(self):
-        return self.model.datetimeDisplay
-
-    @datetime_display.setter
-    def datetime_display(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.datetimeDisplay = value
-        self.db_conn.commit()
-
-    @property
-    def datetime_start(self):
-        return self.model.datetimeStart
-
-    @datetime_start.setter
-    def datetime_start(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.datetimeStart = value
-        self.db_conn.commit()
-
-    @property
-    def datetime_finish(self):
-        return self.model.datetimeFinish
-
-    @datetime_finish.setter
-    def datetime_finish(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.datetimeFinish = value
-        self.db_conn.commit()
-
-    @property
-    def auto_start(self):
-        return self.model.autoStart
-
-    @auto_start.setter
-    def auto_start(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.autoStart = value
-        self.db_conn.commit()
-
-    @property
-    def scoring_engine(self):
-        return self.model.scoringEngine
-
-    @scoring_engine.setter
-    def scoring_engine(self, value):
-        # Todo:
-        # Only site admins & organization admins should be able to change this
-        self.model.scoringEngine = value
-        self.db_conn.commit()
 
     @classmethod
     def from_dict(cls, db_conn, kw_dict):
@@ -348,33 +132,6 @@ class Team(ModelWrapper):
         'scoreConfigurations']
 
     @property
-    def username(self):
-        return self.model.username
-
-    @username.setter
-    def username(self, value):
-        self.model.username = value
-        self.db_conn.commit()
-
-    @property
-    def name(self):
-        return self.model.name
-
-    @name.setter
-    def name(self, value):
-        self.model.name = value
-        self.db_conn.commit()
-
-    @property
-    def password(self):
-        return self.model.password
-
-    @password.setter
-    def password(self, value):
-        self.model.password = value
-        self.db_conn.commit()
-
-    @property
     def network_cidr(self):
         return self.model.networkCidr
 
@@ -415,33 +172,6 @@ class Score(ModelWrapper):
         'value',
         'message']
 
-    @property
-    def datetime(self):
-        return self.model.datetime
-
-    @datetime.setter
-    def datetime(self, value):
-        self.model.datetime = value
-        self.db_conn.commit()
-
-    @property
-    def value(self):
-        return self.model.value
-
-    @value.setter
-    def value(self, value):
-        self.model.value = value
-        self.db_conn.commit()
-
-    @property
-    def message(self):
-        return self.model.message
-
-    @message.setter
-    def message(self, value):
-        self.model.message = value
-        self.db_conn.commit()
-
 class Inject(ModelWrapper):
     'Inject object for controling inject settings'
     model_object = InjectModel
@@ -453,75 +183,6 @@ class Inject(ModelWrapper):
         'datetimeResponseClose',
         'title',
         'body']
-
-    @property
-    def require_response(self):
-        return self.model.requireResponse
-
-    @require_response.setter
-    def require_response(self, value):
-        self.model.requireResponse = value
-        self.db_conn.commit()
-
-    # Manual Delivery modules
-    @property
-    def manual_delivery(self):
-        return self.model.manualDelivery
-
-    @manual_delivery.setter
-    def manual_delivery(self, value):
-        self.model.manualDelivery = value
-        self.db_conn.commit()
-
-    # Datetime Delivery modules
-    @property
-    def datetime_delivery(self):
-        return self.model.datetimeDelivery
-
-    @datetime_delivery.setter
-    def datetime_delivery(self, value):
-        self.model.datetimeDelivery = value
-        self.db_conn.commit()
-
-    # Datetime Response Due modules
-    @property
-    def datetime_response_due(self):
-        return self.model.datetimeResponseDue
-
-    @datetime_response_due.setter
-    def datetime_response_due(self, value):
-        self.model.datetimeResponseDue = value
-        self.db_conn.commit()
-
-    # Datetime Response Close modules
-    @property
-    def datetime_response_close(self):
-        return self.model.datetimeResponseClose
-
-    @datetime_response_close.setter
-    def datetime_response_close(self, value):
-        self.model.datetimeResponseClose = value
-        self.db_conn.commit()
-
-    # Title modules
-    @property
-    def title(self):
-        return self.model.title
-
-    @title.setter
-    def title(self, value):
-        self.model.title = value
-        self.db_conn.commit()
-
-    # Body modules
-    @property
-    def body(self):
-        return self.model.body
-
-    @body.setter
-    def body(self, value):
-        self.model.body = value
-        self.db_conn.commit()
 
     # # Document modules
     # def addDocument(self, fileObj, contentType, filePath, filename, **kwargs):
@@ -551,24 +212,6 @@ class InjectResponse(ModelWrapper):
         'datetime',
         'content']
 
-    @property
-    def datetime(self):
-        return self.model.datetime
-
-    @datetime.setter
-    def datetime(self, value):
-        self.model.datetime = value
-        self.db_conn.commit()
-
-    @property
-    def content(self):
-        return self.model.content
-
-    @content.setter
-    def content(self, value):
-        self.model.content = value
-        self.db_conn.commit()
-
 class Incident(ModelWrapper):
     'Incident object for controlling incident settings'
     model_object = IncidentModel
@@ -577,33 +220,6 @@ class Incident(ModelWrapper):
         'subject',
         'content']
 
-    @property
-    def datetime(self):
-        return self.model.datetime
-
-    @datetime.setter
-    def datetime(self, value):
-        self.model.datetime = value
-        self.db_conn.commit()
-
-    @property
-    def subject(self):
-        return self.model.subject
-
-    @subject.setter
-    def subject(self, value):
-        self.model.subject = value
-        self.db_conn.commit()
-
-    @property
-    def content(self):
-        return self.model.content
-
-    @content.setter
-    def content(self, value):
-        self.model.content = value
-        self.db_conn.commit()
-
 class IncidentResponse(ModelWrapper):
     'Incident Response object for controlling incident response settings'
     model_object = IncidentResponseModel
@@ -611,39 +227,3 @@ class IncidentResponse(ModelWrapper):
         'datetime',
         'subject',
         'content']
-
-    @property
-    def reply_to(self):
-        return self.model.replyTo
-
-    @reply_to.setter
-    def reply_to(self, value):
-        self.model.replyTo
-        self.db_conn.commit()
-
-    @property
-    def datetime(self):
-        return self.model.datetime
-
-    @datetime.setter
-    def datetime(self, value):
-        self.model.replyTo
-        self.db_conn.commit()
-
-    @property
-    def subject(self):
-        return self.model.subject
-
-    @subject.setter
-    def subject(self, value):
-        self.model.subject = value
-        self.db_conn.commit()
-
-    @property
-    def content(self):
-        return self.model.content
-
-    @content.setter
-    def content(self, value):
-        self.model.content = value
-        self.db_conn.commit()
